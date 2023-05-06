c8d6752aaa3571c71faba1cb2f22e7d0c4d642be
Mangasar <mangasarpetrosyan@gmail.com>
2023-05-06 18:14:59 +0300

    task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,10 @@
-const InpLen = document.querySelector('input#validation-input')
+const InpLen = document.querySelector('#validation-input')
+InpLen.addEventListener('blur',() => {
+if (InpLen.value.length == InpLen.dataset.length) {
+   return InpLen.classList.add('valid') , InpLen.classList.remove('invalid')
+}
+return InpLen.classList.add('invalid') , InpLen.classList.remove('valid')
+
+}
+ )
+
