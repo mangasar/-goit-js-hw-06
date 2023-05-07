ad6772d3773f1a88337dac59be56cc2cbb50c0ab
Mangasar <mangasarpetrosyan@gmail.com>
2023-05-07 16:10:45 +0300

    8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form')
+form.addEventListener('submit', onFormSummit )
 
+function onFormSummit (event) {
+event.preventDefault();
+
+
+const {
+     elements : { email, password } 
+  } = event.currentTarget 
+
+
+if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+}
+
+console.log({ email: email.value, password: password.value });
+event.currentTarget.reset();
+} 
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+
+const changeColorEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+const bodyDoc = document.querySelector("body");
+
+changeColorEl.addEventListener("click", changeBackgroundColor);
+function changeBackgroundColor() {
+  return bodyDoc.style.backgroundColor = getRandomHexColor(), colorEl.textContent = getRandomHexColor();
 }
